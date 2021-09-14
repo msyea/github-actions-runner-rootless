44bea98009fbcb22af2f1ebcf9bffd33729909cc
Albert Yanit <ayanit1@googlemail.com>
2021-09-14 08:38:35 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,7 +4,6 @@ deregister_runner() {
   echo "Caught SIGTERM. Deregistering runner"
   _TOKEN=$(token.sh)
   RUNNER_TOKEN=$(echo "${_TOKEN}" | jq -r .token)
-  docker system prune -a -f
   ./config.sh remove --token "${RUNNER_TOKEN}"
   exit
 }
